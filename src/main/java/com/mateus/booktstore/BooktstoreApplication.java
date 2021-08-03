35621dee8797699a3617f07a7208ca32385ce692
Mateus Firmino <mateusfirmino1@hotmail.com>
2021-08-03 17:06:19 -0400

    commit inicial

---
+++ src/main/java/com/mateus/booktstore/BooktstoreApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.booktstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooktstoreApplication.class, args);
+    }
+
+}
